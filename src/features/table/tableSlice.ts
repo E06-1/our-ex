5b49e31cffdddd2d2877be085e2cd6abb78d08c9
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-02-03 22:23:02 +0100

    Add SetCellContent Reducert

--- src/features/table/tableSlice.ts
+++ src/features/table/tableSlice.ts
@@ -1,6 +1,5 @@
-import { RowingSharp } from "@mui/icons-material";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import React from "react";
+import React, { Reducer } from "react";
 import type { RootState } from "../../store";
 
 // Define a type for the slice state
@@ -15,7 +14,7 @@ interface Cell {
   style: React.CSSProperties;
 }
 
-const initialCellState = { content: "", style: {} };
+export const initialCellState = { content: "", style: {} };
 
 // Define the initial state using that type
 export const initialState: Table = {};
@@ -25,19 +24,25 @@ export const tableSlice = createSlice({
   // `tableSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    reset: (
+    init: (
       state,
       action: PayloadAction<{ rows: number; columns: number }>
     ) => {
       const newState = { ...initialState };
-      for (let r = 0; r <= action.payload.rows; r++) {
-        for (let c = 0; c <= action.payload.columns; c++) {
+      for (let r = 0; r < action.payload.rows; r++) {
+        for (let c = 0; c < action.payload.columns; c++) {
           newState[`${r},${c}`] = initialCellState;
         }
       }
       return newState;
     },
     overwrite: (state, action: PayloadAction<Table>) => action.payload,
+    setCellContent: (
+      state,
+      action: PayloadAction<{ cellname: CellName; content: string }>
+    ) => {
+      state[action.payload.cellname].content = action.payload.content;
+    },
     setCellStyle: (
       state,
       action: PayloadAction<{ cellname: CellName; style: React.CSSProperties }>
@@ -67,7 +72,9 @@ export const tableSlice = createSlice({
   },
 });
 
-export const { reset, overwrite, setCellStyle, mergeCellStyle } = tableSlice.actions;
+
+export const { init, overwrite, setCellStyle, mergeCellStyle, setCellContent } =
+  tableSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectCell = (cellName: CellName) => (state: RootState) =>
