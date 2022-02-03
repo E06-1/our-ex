f7fc3e7ff49704e544de09b771bfa1ca8e1f0561
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-02-03 16:48:56 +0100

    add Grid Component

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import './Grid.css';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { CellName, reset, selectTable } from "../../features/table/tableSlice";
+import { useAppDispatch } from "../../store";
+import Cell from "./Cell/Cell";
+import "./Grid.css";
 
+interface GridProps {
+  rows: number;
+  columns: number;
+}
 
-function Grid() {
+function Grid({ rows, columns }: GridProps) {
+  const table = useSelector(selectTable);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(reset({ rows, columns }));
+  }, [rows, columns, dispatch]);
   return (
     <div className="Grid">
+      {Object.keys(table.present).map((cellname, state) => (
+        <Cell cellname={cellname as CellName} />
+      ))}
     </div>
   );
 }
