76651bdacabba63e567767235855d4c10bce3c3f
Simona <semuona@gmail.com>
2022-02-03 14:08:19 +0100

    buttons done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import ActionBar from './components/ActionBar/ActionBar';
+
 
 
 function App() {
   return (
     <div className="App">
+      <ActionBar/>
     </div>
   );
 }
---
+++ src/components/ActionBar/Min/Min.jsx
@@ -0,0 +1,6 @@
+import Button from "@mui/material/Button";
+export default function Min() {
+  // const getMin = (array) => Math.max(...array);
+
+  return <Button variant="contained">Min</Button>;
+}
