76ef871b7ea81289d0910faf055cac8a57e295e4
Simona <semuona@gmail.com>
2022-02-04 16:16:34 +0100

    starting reset

--- src/components/ActionBar/Reset/Reset.jsx
+++ src/components/ActionBar/Reset/Reset.jsx
@@ -1,6 +1,13 @@
 import RestartAltIcon from "@mui/icons-material/RestartAlt";
 import Button from "@mui/material/Button";
+import { useAppDispatch } from "../../../store";
+import { setCellContent } from "../../../features/table/tableSlice";
+import { setCellStyle } from "../../../features/table/tableSlice";
+import tableSlice from "../../../features/table/tableSlice";
+
 export default function Reset() {
+  const dispatch = useAppDispatch();
+
   return (
     <Button variant="contained">
       Reset <RestartAltIcon />{" "}
